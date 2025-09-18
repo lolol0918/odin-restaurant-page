6da7696326ca8733b7ad2e8861b96f9232d8ff35
Derek <derekdvalerio@gmail.com>
2025-09-18 14:08:53 +0800

    added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,6 +10,11 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true, // clears old files in dist before each build
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // take from src
+    }),
+  ],
   devServer: {
     static: "./dist", // tell dev server to serve files from dist
     open: true,       // auto-open browser when running `npx webpack serve`
